9df447fcc81478ba4943b17526333f52b6ecd611
Nguyen Van Phu An <aloistrancy2905@gmail.com>
2024-08-06 17:47:54 +0700

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,28 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckAdmin;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+// Auth::loginUsingId(1);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/users', [UserController::class, 'index'])->name('users.index');
-// Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])
+    ->name('users.create')
+    ->middleware([
+        'admin',
+    ]);
 // Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
 // Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 // Route::post('/users', [UserController::class, 'store'])->name('users.store');
 // Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 // Route::delete('/users/{user}', [UserController::class, 'delete'])->name('users.delete');
 
-Route::resource('/users', TaskController::class);
+// Route::resource('/users', TaskController::class);
 
 Route::resource('/tasks', TaskController::class);
-
-
