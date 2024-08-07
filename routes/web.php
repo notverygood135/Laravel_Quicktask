aeb47ee5732f4738641d02a7fa74d68af4b23972
Nguyen Van Phu An <aloistrancy2905@gmail.com>
2024-08-07 09:54:40 +0700

    Setup authentication using Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,20 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\CheckAdmin;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-// Auth::loginUsingId(1);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])
-    ->name('users.create')
-    ->middleware([
-        'admin',
-    ]);
-// Route::get('/users/edit', [UserController::class, 'edit'])->name('users.edit');
-// Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-// Route::post('/users', [UserController::class, 'store'])->name('users.store');
-// Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-// Route::delete('/users/{user}', [UserController::class, 'delete'])->name('users.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::resource('/users', TaskController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('/tasks', TaskController::class);
+require __DIR__.'/auth.php';
